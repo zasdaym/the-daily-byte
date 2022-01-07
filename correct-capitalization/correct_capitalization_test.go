e66e2ead38184a7da65dd8be2445fc9b7c5222cc
Zasda Yusuf Mikail <zasdaym@gmail.com>
2022-01-07 07:05:27 +0700

    Update Correct Capitalization test

--- correct-capitalization/correct_capitalization_test.go
+++ correct-capitalization/correct_capitalization_test.go
@@ -4,42 +4,57 @@ import "testing"
 
 func TestCorrectCapitalization(t *testing.T) {
 	tests := []struct {
+		name string
 		in   string
 		want bool
 	}{
 		{
+			name: "single char",
 			in:   "a",
 			want: true,
 		},
 		{
-			in:   "US",
-			want: true,
-		},
-		{
-			in:   "uS",
-			want: false,
-		},
-		{
+			name: "all letter capitalized",
 			in:   "USA",
 			want: true,
 		},
 		{
+			name: "first letter capitalized",
 			in:   "Calvin",
 			want: true,
 		},
 		{
+			name: "no capitalization",
+			in:   "coding",
+			want: true,
+		},
+		{
+			name: "half capitalized",
+			in:   "CALVin",
+			want: false,
+		},
+		{
+			name: "wrong caps on second letter",
+			in:   "cOmputer",
+			want: false,
+		},
+		{
+			name: "wrong caps in the middle",
 			in:   "compUter",
 			want: false,
 		},
 		{
-			in:   "coding",
-			want: true,
+			name: "wrong caps in the end",
+			in:   "computeR",
+			want: false,
 		},
 	}
 
 	for _, tt := range tests {
-		if got := CorrectCapitalization(tt.in); got != tt.want {
-			t.Errorf("IsCorrect(%s) = %v, want %v", tt.in, got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			if got := CorrectCapitalization(tt.in); got != tt.want {
+				t.Errorf("IsCorrect(%s) = %v, want %v", tt.in, got, tt.want)
+			}
+		})
 	}
 }
